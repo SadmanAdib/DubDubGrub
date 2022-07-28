385f11bc8202834421580e5188fd569d2c9a50a5
Sadman Adib <sadmanadib67@gmail.com>
2022-07-28 10:46:47 +0600

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Sadman Adib on 28/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
